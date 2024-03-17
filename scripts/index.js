06edb295f7d696932f7692678aa1be367880090d
Primer2022 <107935303+Primer2022@users.noreply.github.com>
2024-03-17 20:48:33 +1000

    Ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,25 @@
 const placesList = document.querySelector('.places__list');
 
-initialCards.forEach((card) => {
-    createCard(card.name, card.link);
+initialCards.forEach((cardData) => {
+    addCardToList(createCard(cardData));
 })
 
-function createCard(cardTitle, cardImageSrc) {
+function createCard(cardData) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__image').setAttribute('src', cardImageSrc);
-    cardElement.querySelector('.card__title').textContent = cardTitle;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
-        cardElement.remove();
-    });
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
 
-    placesList.append(cardElement);
+    return cardElement;
+}
+
+function addCardToList(card) {
+    placesList.append(card);
+}
+
+function removeCard(event) {
+    event.target.parentElement.remove();
 }
